7271dae6f798cc16405d50ad8f1e3e8995534c63
Иванов Владимир Валерьевич <vl.ivanov@2gis.ru>
2023-12-02 22:18:24 +0700

    Day 1 part 2.

--- task1/main.cpp
+++ task1/main.cpp
@@ -33,6 +33,67 @@ int extract_number(std::string_view str)
 	return first * 10 + second;
 }
 
+std::string_view digits[] = {
+	"one",
+	"two",
+	"three",
+	"four",
+	"five",
+	"six",
+	"seven",
+	"eight",
+	"nine",
+};
+
+int check_number(std::string_view str)
+{
+	for (std::size_t i = 0; i < std::size(digits); ++i)
+	{
+		const auto digit = digits[static_cast<std::size_t>(i)];
+		if (str.starts_with(digit))
+		{
+			return i + 1;
+		}
+	}
+	const auto ch = str[0];
+	if ('0' <= ch && ch <= '9')
+	{
+		return ch - '0';
+	}
+	return -1;
+}
+
+int extract_number_v2(std::string_view str)
+{
+	int first = -1;
+	int second = -1;
+	std::size_t idx = 0;
+
+	while (!str.empty())
+	{
+		auto digit = check_number(str);
+		if (digit > 0)
+		{
+			first = digit;
+			break;
+		}
+		str = str.substr(1);
+	}
+	while (!str.empty())
+	{
+		auto digit = check_number(str);
+		if (digit > 0)
+		{
+			second = digit;
+		}
+		str = str.substr(1);
+	}
+	if (first < 0 || second < 0)
+	{
+		return 0;
+	}
+	return first * 10 + second;
+}
 
 int main(int argc, char ** argv)
 {
@@ -46,7 +107,8 @@ int main(int argc, char ** argv)
 	while (!input.bad() && !input.eof())
 	{
 		std::getline(input, line);
-		result += extract_number(line);
+//		result += extract_number(line);
+		result += extract_number_v2(line);
 	}
 	std::cout << result << '\n';
 	return 0;
