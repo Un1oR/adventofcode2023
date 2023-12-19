caa2cea9e86f5e83cf1449779543d679754d58ad
Иванов Владимир Валерьевич <vl.ivanov@2gis.ru>
2023-12-20 04:56:54 +0700

    Day 14 part 2.

--- 14.2.cpp
+++ 14.2.cpp
@@ -1,11 +1,10 @@
 #include "h.h"
 
-#include <algorithm>
 #include <bitset>
 #include <cstdint>
 #include <format>
-#include <functional>
 #include <fstream>
+#include <functional>
 #include <iomanip>
 #include <iostream>
 #include <ranges>
@@ -33,37 +32,163 @@ public:
 	{
 	}
 
-	std::uint64_t get_result() const
+	std::uint64_t get_result(bool debug = false) const
 	{
 		std::uint64_t result = 0;
 		std::size_t current_line = height_;
 		const auto position = std::bind_front(to_position, width_);
 		for (std::size_t i = 0; i < height_; ++i)
 		{
-			std::cout << std::setw(3) << current_line << ": ";
-			for (std::size_t j = 0; j < width_; ++j)
+			if (debug)
 			{
-				auto pos = position(i, j);
-				std::cout << "   " << (map_[pos] ? '#' : (data_[pos] ? 'O' : '.'));
+				std::cout << std::setw(3) << current_line << ": ";
+				for (std::size_t j = 0; j < width_; ++j)
+				{
+					auto pos = position(i, j);
+					std::cout << "   " << (map_[pos] ? '#' : (data_[pos] ? 'O' : '.'));
+				}
+				std::cout << "\n     ";
 			}
-			std::cout << "\n     ";
 			for (std::size_t j = 0; j < width_; ++j)
 			{
 				auto pos = position(i, j);
 				if (!map_[pos] && data_[pos])
 				{
-					std::cout << ' ' << std::setw(3) << current_line;
+					if (debug)
+					{
+						std::cout << ' ' << std::setw(3) << current_line;
+					}
 					result += current_line;
 				}
 				else
 				{
-					std::cout << "    ";
+					if (debug)
+					{
+						std::cout << "    ";
+					}
 				}
 			}
-			std::cout << '\n';
+			if (debug)
+			{
+				std::cout << '\n';
+			}
 			--current_line;
 		}
 		return result;
+	}
+
+	void spin(std::size_t count)
+	{
+		std::unordered_map<std::vector<bool>, std::size_t> cache;
+		std::size_t i = 0;
+		for (; i < count; ++i)
+		{
+			spin();
+			std::cout << std::hash<std::vector<bool>>()(data_) << ' ' << get_result() << '\n';
+			auto it = cache.find(data_);
+			if (it != cache.end())
+			{
+				break;
+			}
+			cache[data_] = i;
+		}
+		std::cout << "i - cache[data_] = " << i - cache[data_] << '\n';
+		std::size_t loop_len = i - cache[data_];
+		std::size_t tail = (count - i - 1) % loop_len;
+		std::cout << "tail = " << tail << '\n';
+		for (const auto & [data, iter] : cache)
+		{
+			if (iter == i - (loop_len - tail))
+			{
+				data_ = data;
+				break;
+			}
+		}
+		std::cout << std::hash<std::vector<bool>>()(data_) << ' ' << get_result() << '\n';
+	}
+
+	void spin()
+	{
+		rotate_to_north();
+		rotate_to_west();
+		rotate_to_south();
+		rotate_to_east();
+	}
+
+private:
+	void rotate_to_north()
+	{
+		const auto position = [width = width_](std::size_t i, std::size_t j)
+		{
+			return i * width + j;
+		};
+		rotate(width_, height_, position, width_);
+	}
+
+	void rotate_to_west()
+	{
+		const auto position = [width = width_](std::size_t j, std::size_t i)
+		{
+			return i * width + j;
+		};
+		rotate(height_, width_, position, 1);
+	}
+
+	void rotate_to_south()
+	{
+		const auto position = [width = width_, height = height_](std::size_t i, std::size_t j)
+		{
+			return (height - i - 1) * width + j;
+		};
+		rotate(width_, height_, position, -width_);
+	}
+
+	void rotate_to_east()
+	{
+		const auto position = [width = width_](std::size_t j, std::size_t i)
+		{
+			return i * width + (width - j - 1);
+		};
+		rotate(height_, width_, position, -1);
+	}
+
+	void rotate(
+		const std::size_t width,
+		const std::size_t height,
+		std::function<std::size_t(std::size_t, std::size_t)> position,
+		int free_diff)
+	{
+		std::vector<std::size_t> free(width, std::string::npos);
+		for (std::size_t i = 0; i < width; ++i)
+		{
+			for (std::size_t j = 0; j < height; ++j)
+			{
+				const auto pos = position(i, j);
+				if (map_[pos])
+				{
+					free[j] = std::string::npos;
+				}
+				else
+				{
+					if (!data_[pos])
+					{
+						if (free[j] == std::string::npos)
+						{
+							free[j] = pos;
+						}
+					}
+					else
+					{
+						if (free[j] != std::string::npos)
+						{
+							data_[free[j]] = true;
+							data_[pos] = false;
+							free[j] += free_diff;
+						}
+					}
+				}
+			}
+		}
 	}
 
 private:
@@ -119,6 +244,7 @@ int task_14_2(std::string_view input_file)
 {
 	std::ifstream input(input_file.data());
 	Processor processor = read_data(input);
+	processor.spin(1'000'000'000);
 	auto result = processor.get_result();
 	std::cout << result << '\n';
 
